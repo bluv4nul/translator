08197b23eee88a908c8628e6f58cda4b09c15a25
Andrew <andrewkafrolov76@gmail.com>
2025-12-17 11:02:04 +0300

    +build

---
+++ build/precache-manifest.e08e6b7fa91006f9df60c042d5e00d69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd30af257ff34c8bcdc7d97345a12e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3b03f7f50955eb32abf",
+    "url": "/static/css/main.02719815.chunk.css"
+  },
+  {
+    "revision": "d801a2d8c565753a472b",
+    "url": "/static/js/2.e6923b96.chunk.js"
+  },
+  {
+    "revision": "e3b03f7f50955eb32abf",
+    "url": "/static/js/main.36ac8a77.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
